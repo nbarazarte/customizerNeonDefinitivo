30fb706dccda70c6a9845b468ecb8f317335d1ec
Neil Barazarte <ezebarazarte@gmail.com>
2021-03-30 00:39:27 -0400

    quite input range

--- formulario/opciones.php
+++ formulario/opciones.php
@@ -1,14 +1,19 @@
 <div class="row g-3">
 
-  <div class="col-sm-8">
+  <div class="col-sm-12">
     <div class="col-12 form-floating mb-3">
       <input type="text" class="form-control" id="rotulo" placeholder="Tu texto" value="Rótulos Metalarte" onkeyup="deshabiltarBotonCart()">  
       <label for="rotulo">Escríbe tu texto aquí</label>
     </div>
   </div>
 
-  <div class="col-sm-4">
-    <label for="alto">Tipo de Letra:</label>
+</div>
+
+<div class="row g-3">
+
+  <div class="col-sm-6">
+
+    <label for="alto">Tipo de letra:</label>
     <select form="letras" class="form-select" id="letra" onchange=""><!-- browser.js ln 274-->  
       <option style="font-family:'Barcelony'; font-size: 25px"value="Barcelony">Barcelony</option>
       <option style="font-family:'BeonMedium'; font-size: 25px"value="BeonMedium">Beon</option>
@@ -26,58 +31,47 @@
       <option style="font-family:'signatura_monoline_scriptRg';font-size: 40px"value="signatura_monoline_scriptRg">Signatura Monoline</option>
       <option style="font-family:'Slim Joe';font-size: 25px"value="SlimJoe">Slim Joe</option>
       <option style="font-family:'that i love you';font-size: 25px"value="thatiloveyou">That i love you</option>
-
     </select>
+
+  </div>
+
+  <div class="col-sm-6">
+    
+    <label for="altoAncho">Tamaño de la letra:</label>
+    <input type="number" step=".001" class="" id="altoAncho" value="11.000">  
+    <input type="hidden" class="form-range" id="alto" value="11.000" readonly="yes"> 
+
   </div>
 
 </div>
 
-<div class="col-sm-12">
-  <div class="col-12 form-floating mb-3">
-
-    <span for="alto">Dimensiones de su rótulo (Alto y Ancho en cm.)</span><br/>
-    <span style="font-size: 10px; color: red">Deslice hasta obtener las medidas deseadas</span>
-  
-  </div>
-</div>
-
-<div class="col-sm-12">
-  <div class="col-12 form-floating mb-3">
-
-    <input type="range" step=".01" class="form-range" id="altoAncho" min="11" max="38.34" value="11">
-    <input type="hidden" class="form-range" id="alto" value="11" readonly="yes"> 
-
-    <!--
-      <input type="number" step=".01" class="form-control" id="alto" placeholder="Tamaño" value="10" onchange="deshabiltarBotonCart()">  
-    -->
-
-  </div>
-</div>
+<br/>
 
 <div class="row g-3">
 
   <div class="col-sm-4">
-    <div class="col-12 form-floating mb-3">
-      <input type="text" class="form-control" id="altura" placeholder="Alto total texto cm" value="11.000" readonly="yes">  
-      <label for="altura">Alto (mín. 11 cm)</label>
-    </div>
+    
+    <label for="altura">Alto (mín. 11 cm)</label>
+    <input type="text" class="" id="altura" placeholder="Alto total texto cm" value="11.000" readonly="yes">    
+    
   </div>
 
   <div class="col-sm-4">
-    <div class="col-12 form-floating mb-3">
-      <input type="text" class="form-control" id="ancho" placeholder="Ancho total texto cm" value="68.853" readonly="yes">
-      <input type="hidden" class="form-control" id="anchoSVG" value="0" readonly="yes">
-      <label for="ancho">Ancho (máx. 240 cm)</label>
-    </div>
+      
+    <label for="ancho">Ancho (máx. 240 cm)</label>
+    <input type="text" class="" id="ancho" placeholder="Ancho total texto cm" value="68.853" readonly="yes">
+    <input type="hidden" class="form-control" id="anchoSVG" value="0" readonly="yes">
+
   </div>
 
   <div class="col-sm-4">
+
     <label for="alto">Tiempos de Entrega:</label>
     <select for="tiempos" class="form-select" id="tiempos" onchange="deshabiltarBotonCart()">  
-      
       <option value="<?php echo esc_html($cn_precio_sietediaslaborales);?>">7 días (laborales)</option>
       <option value="<?php echo esc_html($cn_precio_4872);?>">48 a 72 horas</option>
     </select>
+
   </div>
 
 </div>
